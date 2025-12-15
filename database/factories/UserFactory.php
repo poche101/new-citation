05e10aa7fddf3e16f0149ce31fe9f4f28e269616
poche101 <pochetechofficial@gmail.com>
2025-12-15 08:30:19 +0100

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,19 +3,24 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
+    // Specify the corresponding model if not already set
+    protected $model = \App\Models\User::class;
+
+    /**
+     * Define the model's default state.
+     */
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->name(),            // use $this->faker
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => bcrypt('password'), // password
-            'remember_token' => \Str::random(10),
+            'password' => bcrypt('password'),          // password
+            'remember_token' => Str::random(10),
         ];
     }
 }
